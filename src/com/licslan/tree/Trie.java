aa9ea8bfa9888370b636509208239d5a3aafa25f
licslan <licslan@sina.com>
2020-04-06 12:24:26 +0800

    trie test

--- src/com/licslan/tree/Trie.java
+++ src/com/licslan/tree/Trie.java
@@ -14,6 +14,10 @@ package com.licslan.tree;
  * treeMap   //红黑树底层
  * */
 
+import com.licslan.setmap.BSTSet;
+import com.licslan.setmap.FileOperation;
+
+import java.util.ArrayList;
 import java.util.TreeMap;
 public class Trie {
 
@@ -62,5 +66,61 @@ public class Trie {
         }
     }
 
+
+    //查询单词word是否包含在tire中
+    public boolean contains(String word){
+        Node cur=root;
+        for(int i=0;i<word.length();i++){
+            char c = word.charAt(i);
+            if(cur.next.get(c)==null)return false;
+            cur=cur.next.get(c);
+        }
+        return cur.isWord;
+    }
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Pride and Prejudice");
+
+        ArrayList<String> words = new ArrayList<>();
+        if(FileOperation.readFile("pride-and-prejudice.txt", words)){
+
+            long startTime = System.nanoTime();
+
+            BSTSet<String> set = new BSTSet<>();
+            for(String word: words)
+                set.add(word);
+
+            for(String word: words)
+                set.contains(word);
+
+            long endTime = System.nanoTime();
+
+            double time = (endTime - startTime) / 1000000000.0;
+
+            System.out.println("Total different words: " + set.getSize());
+            System.out.println("BSTSet: " + time + " s");
+
+            // ---
+
+            startTime = System.nanoTime();
+
+            Trie trie = new Trie();
+            for(String word: words)
+                trie.add(word);
+
+            for(String word: words)
+                trie.contains(word);
+
+            endTime = System.nanoTime();
+
+            time = (endTime - startTime) / 1000000000.0;
+
+            System.out.println("Total different words: " + trie.getSize());
+            System.out.println("Trie: " + time + " s");
+        }
+    }
 
 }
